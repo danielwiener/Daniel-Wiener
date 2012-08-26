c3a59ea56e545a60f0037be96bff3eb296a10e2d
Daniel Wiener <daniel@danielwiener.com>
2012-08-26 14:01:53 -0400

    using bloginfo instead of listing whole url

--- themes/danielwiener/header.php
+++ themes/danielwiener/header.php
@@ -35,7 +35,7 @@
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<link rel="stylesheet" type="text/css" media="all" href="http://danielwiener.dev/is/wp-content/themes/danielwiener/css/tabs_slideshow.css" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_directory'); ?>/css/tabs_slideshow.css" />
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
