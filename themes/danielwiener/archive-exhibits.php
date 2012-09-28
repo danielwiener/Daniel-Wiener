9f769ccbdb551413d7befa496a717c0e946a3959
Daniel Wiener <daniel@danielwiener.com>
2012-09-27 21:37:53 -0400

    creating an include for grabbing the exhibit variable - terms and metaboxes

--- themes/danielwiener/archive-exhibits.php
+++ themes/danielwiener/archive-exhibits.php
@@ -22,7 +22,7 @@ get_header(); ?>
 				<?php rewind_posts(); ?>
 
 				<?php _s_content_nav( 'nav-above' ); ?>
-
+				<?php include('inc/exhibits_functions.php'); ?>
 				<?php 
 				global $wp_query;
 					$args = array_merge( $wp_query->query,
@@ -49,50 +49,7 @@ get_header(); ?>
 							<?php the_post_thumbnail('thumbnail', array('class' => 'alignleft', 'title' => get_the_title() )); ?>
 							<?php endif; ?>
 								</a>
-							<?php $exhibits_duration =  get_post_meta($post->ID, "_dw_duration", $single = true);?>
-							<?php $exhibits_opening =  get_post_meta($post->ID, "_dw_opening", $single = true) ? '<li>Opening: ' . get_post_meta($post->ID, "_dw_opening", $single = true) . '</li>' : ''; ?>
-							<?php $exhibits_curator =  get_post_meta($post->ID, "_dw_curated_by", $single = true) ? '<li>&nbsp;</li><li>' . get_post_meta($post->ID, "_dw_curated_by", $single = true) . '</li>' : ''; ?>
-							<?php $venue_list = wp_get_post_terms($post->ID, 'venues', array("fields" => "all"));
-														 								foreach ($venue_list as $venue) {
-														 								$venue_name = $venue->name;
-																						$venue_slug = $venue->slug;
-																						$venue_id = $venue->term_id;
-																						$venue_meta =  get_option("taxonomy_$venue_id"); 
-																						$venue_address = $venue_meta['dw_venue_address'];
-																						$venue_phone = $venue_meta['dw_venue_phone'];
-																						$venue_url = $venue_meta['dw_venue_url'];
-																						$venue_zip = $venue_meta['dw_venue_zip'];
-														 								}
-							$city_list = wp_get_post_terms($post->ID, 'dw_cities', array("fields" => "all"));
-								foreach ($city_list as $dw_city) {
-									$dw_city_name = $dw_city->name;
-									$dw_city_slug = $dw_city->slug;
-									$dw_city_id = $dw_city->term_id;
-									$dw_city_count = $dw_city->count;
-								}
-							$state_list = wp_get_post_terms($post->ID, 'dw_states', array("fields" => "all"));
-								foreach ($state_list as $dw_state) {
-									$dw_state_name = $dw_state->name;
-									$dw_state_slug = $dw_state->slug;
-									$dw_state_id = $dw_state->term_id;
-									$dw_state_count = $dw_state->count;
-								}
-							$years_list = wp_get_post_terms($post->ID, 'dw_years', array("fields" => "all"));
-								foreach ($years_list as $dw_year) {
-									$dw_year_name = $dw_year->name;
-									$dw_year_slug = $dw_year->slug;
-									$dw_year_id = $dw_year->term_id;
-									$dw_year_count = $dw_year->count;
-								}
-							$exhibit_types = wp_get_post_terms($post->ID, 'dw_exhibit_types', array("fields" => "all"));
-								foreach ($exhibit_types as $exhibit_type) {
-									$exhibit_type_name = $exhibit_type->name;
-									$exhibit_type_slug = $exhibit_type->slug;
-									$exhibit_type_id = $exhibit_type->term_id;
-									$exhibit_type_count = $exhibit_type->count;
-
-								}
-							?>
+							
 							<small><a href="/is/exhibit_type/<?php echo $exhibit_type_slug ?>">[<?php echo $exhibit_type->name; ?>]</a></small>
 							<ul>
 							<li><?php echo $exhibits_duration; ?>, <a href="/is/years/<?php echo $dw_year_slug; ?>"><?php echo $dw_year_name ?></a></li>
--- themes/danielwiener/single-exhibits.php
+++ themes/danielwiener/single-exhibits.php
@@ -12,7 +12,7 @@ get_header(); ?>
 
 		<section id="primary" class="site-content">
 			<div id="content" role="main">
-
+			<?php include('inc/exhibits_functions.php'); ?>
 			<?php if ( have_posts() ) : ?>
 
 				<header class="page-header">
@@ -21,19 +21,7 @@ get_header(); ?>
 
 				<?php rewind_posts(); ?>
 
-				<?php //_s_content_nav( 'nav-above' ); ?>
 
-				<?php 
-				// global $wp_query;
-				// 		$args = array_merge( $wp_query->query,
-				// 			array(
-				// 				'orderby'         => 'meta_value',
-				// 				'meta_key'		  => '_dw_begin_date',
-				// 				'order'           => 'DESC', 
-				// 			)	
-				// 		); 
-				//  	query_posts( $args );
-				/* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 
 					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -44,50 +32,7 @@ get_header(); ?>
 
 
 						<div class="entry-summary">
-							<?php $exhibits_duration =  get_post_meta($post->ID, "_dw_duration", $single = true);?>
-							<?php $exhibits_opening =  get_post_meta($post->ID, "_dw_opening", $single = true) ? '<li>Opening: ' . get_post_meta($post->ID, "_dw_opening", $single = true) . '</li>' : ''; ?>
-							<?php $exhibits_curator =  get_post_meta($post->ID, "_dw_curated_by", $single = true) ? '<li>&nbsp;</li><li>' . get_post_meta($post->ID, "_dw_curated_by", $single = true) . '</li>' : ''; ?>
-							<?php $venue_list = wp_get_post_terms($post->ID, 'venues', array("fields" => "all"));
-														 								foreach ($venue_list as $venue) {
-														 								$venue_name = $venue->name;
-																						$venue_slug = $venue->slug;
-																						$venue_id = $venue->term_id;
-																						$venue_meta =  get_option("taxonomy_$venue_id"); 
-																						$venue_address = $venue_meta['dw_venue_address'];
-																						$venue_phone = $venue_meta['dw_venue_phone'];
-																						$venue_url = $venue_meta['dw_venue_url'];
-																						$venue_zip = $venue_meta['dw_venue_zip'];
-														 								}
-							$city_list = wp_get_post_terms($post->ID, 'dw_cities', array("fields" => "all"));
-								foreach ($city_list as $dw_city) {
-									$dw_city_name = $dw_city->name;
-									$dw_city_slug = $dw_city->slug;
-									$dw_city_id = $dw_city->term_id;
-									$dw_city_count = $dw_city->count;
-								}
-							$state_list = wp_get_post_terms($post->ID, 'dw_states', array("fields" => "all"));
-								foreach ($state_list as $dw_state) {
-									$dw_state_name = $dw_state->name;
-									$dw_state_slug = $dw_state->slug;
-									$dw_state_id = $dw_state->term_id;
-									$dw_state_count = $dw_state->count;
-								}
-							$years_list = wp_get_post_terms($post->ID, 'dw_years', array("fields" => "all"));
-								foreach ($years_list as $dw_year) {
-									$dw_year_name = $dw_year->name;
-									$dw_year_slug = $dw_year->slug;
-									$dw_year_id = $dw_year->term_id;
-									$dw_year_count = $dw_year->count;
-								}
-							$exhibit_types = wp_get_post_terms($post->ID, 'dw_exhibit_types', array("fields" => "all"));
-								foreach ($exhibit_types as $exhibit_type) {
-									$exhibit_type_name = $exhibit_type->name;
-									$exhibit_type_slug = $exhibit_type->slug;
-									$exhibit_type_id = $exhibit_type->term_id;
-									$exhibit_type_count = $exhibit_type->count;
-
-								}
-							?><ul class="flush_left">
+						<ul class="flush_left">
 							<li><small><a href="/is/exhibit_type/<?php echo $exhibit_type_slug ?>">[<?php echo $exhibit_type->name; ?>]</a></small></li>
 							
 							<li><?php echo $exhibits_duration; ?>, <a href="/is/years/<?php echo $dw_year_slug; ?>"><?php echo $dw_year_name ?></a></li>
