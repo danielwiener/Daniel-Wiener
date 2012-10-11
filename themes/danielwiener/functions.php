a5ed870c66f0df2c5f907048ce023c08c351ff01
Daniel Wiener <daniel@danielwiener.com>
2012-10-11 10:47:26 -0400

    correct message for slideshow

--- themes/danielwiener/functions.php
+++ themes/danielwiener/functions.php
@@ -255,6 +255,23 @@ $messages['exhibits'] = array(
 10 => sprintf( __('Exhibit draft updated. <a target="_blank" href="%s">Preview Exhibit</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
 );
 
+$messages['portfolio_slideshow'] = array(
+0 => '', // Unused. Messages start at index 1.
+1 => sprintf( __('Slide Show updated. <a href="%s">View Slide Show</a>'), esc_url( get_permalink($post_ID) ) ),
+2 => __('Custom field updated.'),
+3 => __('Custom field deleted.'),
+4 => __('Slide Show updated.'),
+/* translators: %s: date and time of the revision */
+5 => isset($_GET['revision']) ? sprintf( __('Slide Show restored to revision from %s'), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+6 => sprintf( __('Slide Show published. <a href="%s">View Slide Show</a>'), esc_url( get_permalink($post_ID) ) ),
+7 => __('Slide Show saved.'),
+8 => sprintf( __('Slide Show submitted. <a target="_blank" href="%s">Preview Slide Show</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
+9 => sprintf( __('Slide Show scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview Slide Show</a>'),
+  // translators: Publish box date format, see http://php.net/date
+  date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ), esc_url( get_permalink($post_ID) ) ),
+10 => sprintf( __('Slide Show draft updated. <a target="_blank" href="%s">Preview Slide Show</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
+);
+
    return $messages;
  }
 /**
