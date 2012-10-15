5214c5d2b045b8bc6c3da1b80385fb01e98bdf5f
Daniel Wiener <daniel@danielwiener.com>
2012-10-15 14:42:22 -0400

    using metabox to show all post types on the front page

--- themes/danielwiener/content-front_page.php
+++ themes/danielwiener/content-front_page.php
@@ -11,33 +11,43 @@
 		$args = array(
 			'post_type' 		=> array('titles', 'exhibits', 'post', 'page', 'sculptors'),
 			'post_status' 		=> 'publish',			
-			'meta_query' => array(
-											array(
-												'key' => '_dw_is_front_page_slides',
-												'value' => 'on',
-											)
-										),
-						// 'orderby'         => 'meta_value',
-						// 								'meta_key'		  => '_dw_slide_order',
-						// 								'order'           => 'ASC',
+			// 'meta_query' => array(
+			// 											array(
+			// 												'key' => '_dw_is_front_page_slides',
+			// 												'value' => 'on',
+			// 											)
+			// 										),
+			/*
+				TODO doing multiple queries to a meta_value seems to be overly complicated. so I have just used orderby which selects anything I have given a menu order to. It makes it kind of wonky in the interface. Look back at this sometime. check this out - http://wordpress.stackexchange.com/questions/68002/complex-meta-query-with-3-keys
+			*/
+			'orderby'         => 'meta_value',
+			'meta_key'		  => '_dw_slide_order',
+			'order'           => 'ASC',
 		);
 		$front_query = New WP_Query($args);
 		$slidetabs = ''; ?>
 		<?php while ( 	$front_query->have_posts() ) : 	$front_query->the_post(); ?>
-			<?php $current_category = get_the_category($post->ID); ?>
+			<?php $current_category = get_the_category($post->ID); 
+			if (!$current_category) {
+				$this_post_type = get_post_type_object($post->post_type);
+				$category_display_name = $this_post_type->labels->name;
+			} else {
+				$category_display_name = $current_category[0]->cat_name;
+			}
+			if ($category_display_name == 'Sculptors') {
+				$category_display_name = 'Writing about Sculptors';
+			}
+			?>
+		
 
 		<div>
 					<?php if(has_post_thumbnail()): ?>
 						<a href="<?php the_permalink(); ?>"  class="slide_container"><?php the_post_thumbnail('full'); ?></a>
 						<?php endif; ?>
-						<h4><?php echo $current_category[0]->cat_name; ?> >> <a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"> <?php the_title(); ?><?php if(get_post_meta($post->ID, "Date", $single = true) != "") {
-						$visiting_artist_date = get_post_meta($post->ID, "Date", $single = true);
-						echo ', ' . $visiting_artist_date;
-						$formatted_date = strtotime($visiting_artist_date);
-						}
-						?></a></h4>
+						<h4><?php echo $category_display_name; ?> >> <a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"> <?php the_title(); ?></a></h4>
 					</div><!-- div no class -->
 			<?php $slidetabs .= '<a href="#"></a>'; ?>
+	
 				<?php endwhile; // End the loop.
 				wp_reset_query();?></div><!-- images -->
 
@@ -71,3 +81,4 @@
 	setTimeout('$(".slidetabs").data("slideshow").play();', 2000);
 	});
 	</script>
+	
