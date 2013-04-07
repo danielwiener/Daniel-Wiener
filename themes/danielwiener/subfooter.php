d3c9befa7ede5bc8c96fc3cca04314b6cc09155a
Daniel Wiener <daniel@danielwiener.com>
2013-04-07 09:48:44 -0400

    changing subfooter to show exhibitions automatically

--- themes/danielwiener/subfooter.php
+++ themes/danielwiener/subfooter.php
@@ -43,23 +43,54 @@ t 212 410 6120</p>
 
 <div class="column1">
 <h1><a href="<?php echo bloginfo('siteurl') . '/exhibits'; ?>">Exhibitions</a></h1>
-<h2><a href="http://www.illearts.com/exhibition_SeatingArrange.php">Seating Arrangements</a></h2>
-<p><strong>curated by Mary Heilmann</strong> featuring works by Don Christensen, Mary Heilmann, Kurt Gumaer and Daniel Wiener</p>
+<?php 
+	$args = array(
+			'post_type'		  => 'exhibits',
+			'orderby'         => 'meta_value',
+			'meta_key'		  => '_dw_begin_date',
+			'order'           => 'DESC', 
+	
+	);
+	$exhibit_query = New WP_Query($args); 
 
-<p>August 11 - September 11, 2012</p>
+/* Start the Loop */ ?>
+<?php while ( $exhibit_query->have_posts() ) : $exhibit_query->the_post(); ?>
+<?php if ( get_post_meta($post->ID, "_dw_is_in_subfooter", $single = true)): ?>
+<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 
-<p><a href="http://www.illearts.com/">ILLE Arts</a>
-216a Main Street<br>
-Amagansett, NY<br>
-11930</p>
+	
 
-<h2><a title="LongHouse Reserve" href="http://longhouse.org/exhibitions.ihtml?id=42" target="_blank">Diversities of Sculpture/Derivations from Nature</a></h2>
-<p>curated by Bonnie Rychlak<br />
-April 28th &#8211; October 7th, 2012</p>
-<p><a title="LongHouse Reserve" href="http://longhouse.org/" target="_blank">LongHouse Reserve 2012</a><br>
-133 Hands Creek Road<br />
-East Hampton, New York 11937<br />
-Please check the website for hours and directions.</p>
+
+<?php include('inc/exhibits_functions.php'); ?>
+
+<ul>
+								<li><?php echo $exhibits_duration; ?>, <a href="/is/years/<?php echo $dw_year_slug; ?>"><?php echo $dw_year_name ?></a></li>
+								<?php echo $exhibits_opening; ?>
+								<?php echo $exhibits_curator; ?>
+									<li><?php if ($venue_url): ?>
+									<a href="<?php echo $venue_url ?>">	
+									<?php endif ?>
+								<?php echo $venue_name; ?>
+									<?php if ($venue_url): ?>
+										</a>
+									<?php endif ?></li>
+								<li><?php echo $venue_address; ?></li>
+								<?php $dw_state_display = ($dw_state_count > 1) ? '<a href="/is/state/' . $dw_state_slug . '">' . $dw_state_name . '</a>'  : $dw_state_name ; ?>
+								<?php $dw_city_display = ($dw_city_count > 1) ? '<a href="/is/city/' . $dw_city_slug . '">' . $dw_city_name . '</a>'  : $dw_city_name ; ?>
+								<li><?php echo $dw_city_display; ?>, <?php echo $dw_state_display; ?> <?php echo $venue_zip; ?></li>
+								<li><?php echo $venue_phone; ?></li>
+	</ul>
+								<?php if (get_post_meta($post->ID, "_dw_related_text", $single = true)): ?>
+								<?php
+								$exhibits_related_text =  get_post_meta($post->ID, "_dw_related_text", $single = true);
+								$exhibits_related_text = wpautop($exhibits_related_text, true);
+								echo $exhibits_related_text; ?>	
+								<?php endif ?>
+<?php endif ?>
+<?php endwhile; ?>
+
+
+
 <h1>Materials</h1>
 <h2><a href="http://www.avesstudio.com/index.php?page=shop.product_details&product_id=28&flypage=flypage.tpl&pop=0&option=com_virtuemart&Itemid=4&vmcchk=1&Itemid=4" target="_blank">Apoxie Sculpt</a></h2>
 <p>Apoxie-Sculpt is a self-hardening clay manufactured by Aves Studios.</p>
